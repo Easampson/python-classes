95b3c63ab22d780c0cf3437c9d5b5e2e97a4942a
Edward Sampson <edwardsampson@gmail.com>
2016-11-22 10:09:19 -0500

    added merge and shell classes

---
+++ mergeSort_class.py
@@ -0,0 +1,35 @@
+def mergeSort(alist):
+    print("SPLIT ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+        while j < len(righthalf):
+            j = j + 1
+            k = k + 1
+    print ("merging ", alist)
+
+
+# test working: NO
+# alist = [21,32,4,34,65,32,2,1,5,54,65,676,5,34]
+# mergeSort(alist)
+# print(alist)
+
